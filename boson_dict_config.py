5efc918701a18aacedc07f90a8bc700e1e0fff47
Zhangpeixiang <1321444999@qq.com>
2019-07-05 13:48:59 +0800

    dir

--- boson_dict_config.py
+++ boson_dict_config.py
@@ -1,9 +1,9 @@
 class bonson_config:
-    stop_word_dir = 'C:\Dissertation\stop.txt'
+    stop_word_dir = './data/stop.txt'
     sentiment_dir1 = './boson_dict_utils/正面评价词语（中文）1.txt',
     sentiment_dir2 = './boson_dict_utils/正面情感词语（中文）1.txt',
     sentiment_dir3 = './boson_dict_utils/负面评价词语（中文）1.txt',
     sentiment_dir4 = './boson_dict_utils/负面情感词语（中文）1.txt',
-    data = 'C:\Dissertation\全网筛选数据-联通-汇总-1113.xlsx'
+    data = './data/data.xlsx'
     boson_dict_dir = './boson_dict_utils/BosonNLP_sentiment_score.txt'
     negitive_words_dir = './boson_dict_utils/否定词.txt'
